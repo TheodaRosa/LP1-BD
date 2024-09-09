1e04f800726bb1340da71c5a77d6e2ceaa880c35
TheodaRosa <theo.smidt@fatec.sp.gov.br>
2024-09-09 19:49:26 -0300

    Calculadora de Frações

--- Exercícios 09.09/Calculadora/src/Main.java
+++ Exercícios 09.09/Calculadora/src/Main.java
@@ -1,22 +1,89 @@
-public class Main {
-    private int numerador1, numerador2, denominador1, denominador2;
+import java.util.Scanner;
 
-    public Main(int numerador1, int numerador2, int denominador1, int denominador2) {
-        this.numerador1 = numerador1;
-        this.numerador2 = numerador2;
-        this.denominador1 = denominador1;
-        this.denominador2 = denominador2;
+class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Denominador não pode ser zero.");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+        reduce();
     }
 
-    public double Fracao1() {
-        return (double) numerador1 / denominador1;
-    }
-    public double Fracao2() {
-        return (double) numerador2 / denominador2;
-    }
-
-    public static void main(String[] args) {
-
+    private void reduce() {
+        int gcd = gcd(numerator, denominator);
+        numerator /= gcd;
+        denominator /= gcd;
     }
 
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public Fraction add(Fraction other) {
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction subtract(Fraction other) {
+        int newNumerator = this.numerator * other.denominator - other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction multiply(Fraction other) {
+        int newNumerator = this.numerator * other.numerator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction divide(Fraction other) {
+        if (other.numerator == 0) {
+            throw new ArithmeticException("Não se pode dividir por zero.");
+        }
+        int newNumerator = this.numerator * other.denominator;
+        int newDenominator = this.denominator * other.numerator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entre o numerador e o denominador para a primeira fração:");
+        int numerator1 = scanner.nextInt();
+        int denominator1 = scanner.nextInt();
+        Fraction fraction1 = new Fraction(numerator1, denominator1);
+
+        System.out.println("Entre o numerador e o denominador para a seguda fração:");
+        int numerator2 = scanner.nextInt();
+        int denominator2 = scanner.nextInt();
+        Fraction fraction2 = new Fraction(numerator2, denominator2);
+
+        System.out.println("First Fraction: " + fraction1);
+        System.out.println("Second Fraction: " + fraction2);
+
+        System.out.println("Soma: " + fraction1.add(fraction2));
+        System.out.println("Subtração: " + fraction1.subtract(fraction2));
+        System.out.println("Multiplicação: " + fraction1.multiply(fraction2));
+        System.out.println("Divisão: " + fraction1.divide(fraction2));
+
+        scanner.close();
+    }
+}
+
